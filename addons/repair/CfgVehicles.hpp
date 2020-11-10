ebc41e66f4129a7bcc48ded203f8fa8a862dd993
Björn Dahlgren <bjorn@dahlgren.at>
2020-11-10 15:47:22 +0100

    Add repair vehicles

---
+++ addons/repair/CfgVehicles.hpp
@@ -0,0 +1,7 @@
+class CfgVehicles {
+    class sfp_tgb30_base;
+    class sfp_tgb30_repair : sfp_tgb30_base {
+        transportRepair = 0;
+        ACE_repair_canRepair = 60000;
+    };
+};
---
+++ addons/repair/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class sfp_ace_compat_repair {
+        author = "Swedish Forces Pack";
+        requiredAddons[] = {"ace_repair", "Swedish_Forces_Pack", "sfp_tgb40"};
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgVehicles.hpp"
