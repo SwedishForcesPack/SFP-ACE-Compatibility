a403184087b5ff3d1402e2bd2b50c6f3c8fac652
Björn Dahlgren <bjorn@dahlgren.at>
2020-11-10 15:41:30 +0100

    Add medical vehicles

---
+++ addons/medical/CfgVehicles.hpp
@@ -0,0 +1,17 @@
+class CfgVehicles {
+	class sfp_hkp16;
+    class sfp_hkp16_medevac : sfp_hkp16 {
+    	ACE_medical_medicClass = 1;
+    };
+
+    class sfp_patgb203;
+    class sfp_patgb203_ambulance : sfp_patgb203 {
+        ACE_medical_medicClass = 1;
+    };
+
+    class sfp_tgb13_ksp58;
+    class sfp_tgb1314 : sfp_tgb13_ksp58
+    {
+        ACE_medical_medicClass = 1;
+    };
+};
---
+++ addons/medical/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class sfp_ace_compat_medical {
+        author = "Swedish Forces Pack";
+        requiredAddons[] = {"ace_medical", "Swedish_Forces_Pack", "sfp_hkp16", "sfp_patgb203", "sfp_tgb11"};
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgVehicles.hpp"
