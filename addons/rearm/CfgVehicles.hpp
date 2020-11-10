cb2855e6918dc62978aefbedc71cbabef7d63b43
Björn Dahlgren <bjorn@dahlgren.at>
2020-11-10 15:49:46 +0100

    Add rearm vehicles

---
+++ addons/rearm/CfgVehicles.hpp
@@ -0,0 +1,13 @@
+class CfgVehicles {
+    class sfp_tgb40_base;
+    class sfp_tgb40_ammo : sfp_tgb40_base {
+        transportAmmo = 0;
+        ace_rearm_defaultSupply = 1200;
+    };
+
+    class sfp_ammobox_container;
+    class sfp_tgb40_ammobox : sfp_ammobox_container {
+        transportAmmo = 0;
+        ace_rearm_defaultSupply = 1200;
+    };
+};
---
+++ addons/rearm/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class sfp_ace_compat_rearm {
+        author = "Swedish Forces Pack";
+        requiredAddons[] = {"ace_rearm", "Swedish_Forces_Pack", "sfp_tgb40"};
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgVehicles.hpp"
