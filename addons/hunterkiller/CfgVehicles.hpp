2051c555c1bc46dbb15314a1ce503c8d1d012608
Björn Dahlgren <bjorn@dahlgren.io>
2022-09-16 13:07:12 +0200

    Enable ACE hunterkiller for Strv 121 and Strv 122

---
+++ addons/hunterkiller/CfgVehicles.hpp
@@ -0,0 +1,11 @@
+class CfgVehicles {
+    class Tank_F;
+
+    class sfp_strv121_base: Tank_F {
+        ace_hunterkiller = 1;
+    };
+
+    class sfp_strv122_base : Tank_F {
+        ace_hunterkiller = 1;
+    };
+};
---
+++ addons/hunterkiller/config.hpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+    class sfp_ace_compat_hunterkiller {
+        author = "Swedish Forces Pack";
+        addonRootClass = "Swedish_Forces_Pack";
+        requiredAddons[] = {"ace_hunterkiller", "Swedish_Forces_Pack", "sfp_strv121", "sfp_strv122"};
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgVehicles.hpp"
