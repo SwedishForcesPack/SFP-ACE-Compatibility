f600c8e57e8c0764b3a2b01325a45af2e8aa4415
Björn Dahlgren <bjorn@dahlgren.io>
2022-09-18 22:29:42 +0200

    Add HunterKiller functionality to Ikv91

--- addons/hunterkiller/CfgVehicles.hpp
+++ addons/hunterkiller/CfgVehicles.hpp
@@ -1,6 +1,10 @@
 class CfgVehicles {
     class Tank_F;
 
+    class sfp_ikv91_base: Tank_F {
+        ace_hunterkiller = 1;
+    };
+
     class sfp_strv121_base: Tank_F {
         ace_hunterkiller = 1;
     };
