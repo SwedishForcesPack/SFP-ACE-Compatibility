a1f92400b90c8c9211dd302189a5bee60e74f8ad
Björn Dahlgren <bjorn@dahlgren.io>
2022-10-16 21:14:56 +0200

    Add ACE dragging and carrying for SFP ammo crates

---
+++ addons/dragging/CfgVehicles.hpp
@@ -0,0 +1,20 @@
+class CfgVehicles {
+    class ReammoBox_F;
+
+    class sfp_ammobox_container : ReammoBox_F {};
+
+    class sfp_ammocrate : sfp_ammobox_container {
+        ace_dragging_canCarry = 1;
+        ace_dragging_canDrag = 1;
+    };
+
+    class sfp_ammocrate_556: sfp_ammobox_container {
+        ace_dragging_canCarry = 1;
+        ace_dragging_canDrag = 1;
+    };
+
+    class sfp_ammocrate_127: sfp_ammobox_container {
+        ace_dragging_canCarry = 1;
+        ace_dragging_canDrag = 1;
+    };
+};
---
+++ addons/dragging/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+    class sfp_ace_compat_dragging {
+        author = "Swedish Forces Pack";
+        addonRootClass = "Swedish_Forces_Pack";
+        requiredAddons[] = {"ace_dragging", "Swedish_Forces_Pack", "sfp_ammocrates"};
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgVehicles.hpp"
